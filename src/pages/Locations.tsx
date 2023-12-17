718868ca7d14e284bf69b18e6f6bc95c54b5c8cf
Mikael Rönnberg <mikael.rnnberg@gmail.com>
2023-12-17 15:16:14 +0100

    build: added chakra-ui, colors and fonts

--- src/pages/Locations.tsx
+++ src/pages/Locations.tsx
@@ -1,7 +1,21 @@
+import { Box, Flex, Heading } from "@chakra-ui/react";
+
 export const Locations = () => {
   return (
     <>
       <h2>Location</h2>
+      <Flex w="100vw" h="100vh" justify="center" align="center">
+        <Flex
+          justify="center"
+          align="center"
+          w="90vw"
+          h="80vh"
+          border="black 2px solid"
+        >
+          <Box></Box>
+          <Heading>Set a Location</Heading>
+        </Flex>
+      </Flex>
     </>
   );
 };
