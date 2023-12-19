1766dcf5ad146f7180d24bd6bf040dc7a2ccc584
Mikael Rönnberg <mikael.rnnberg@gmail.com>
2023-12-19 20:26:44 +0100

    fix: setting up firebase again

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -10,13 +10,13 @@ import { getFirestore } from "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: process.env.REAC_APP_FIREBASE_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_FIREBASE_APPID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
+  apiKey: import.meta.env.VITE_APP_FIREBASE_KEY,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APP_FIREBASE_APPID,
+  measurementId: import.meta.env.VITE_APP_FIREBASE_MEASUREMENTID,
 };
 
 // Initialize Firebase
--- src/pages/admin/feature/Auth.tsx
+++ src/pages/admin/feature/Auth.tsx
@@ -1,32 +1,82 @@
 import { Button, Flex, HStack, Heading, Input, VStack } from "@chakra-ui/react";
-import { auth, googleProvider } from "../../../config/firebase";
+import {
+  auth,
+  // googleProvider
+} from "../../../config/firebase";
 import {
   createUserWithEmailAndPassword,
-  signInWithPopup,
+  // signInWithPopup,
   signOut,
 } from "firebase/auth";
 import { useState } from "react";
 import { loginContainerStyles } from "../style/styleAdmin";
+import { useFormik } from "formik";
+
+interface FormValues {
+  email: string;
+  password: string;
+}
 
 export const Auth = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const signIn = async () => {
-    try {
-      await createUserWithEmailAndPassword(auth, email, password);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  const formik = useFormik<FormValues>({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validate: (values) => {
+      const errors: Partial<FormValues> = {};
+      if (!values.email) {
+        errors.email = "Required";
+      } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+      ) {
+        errors.email = "Invalid email address";
+      }
 
-  const signInWithGoogle = async () => {
-    try {
-      await signInWithPopup(auth, googleProvider);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+      if (!values.password) {
+        errors.password = "Required";
+      }
+
+      return errors;
+    },
+
+    onSubmit: async (values) => {
+      try {
+        await createUserWithEmailAndPassword(
+          auth,
+          values.email,
+          values.password
+        );
+        setErrorMessage("");
+      } catch (error) {
+        if (error instanceof Error) {
+          console.error(error);
+          setErrorMessage(error.message); // This is now safe
+        } else {
+          console.error("An unknown error occurred:", error);
+          setErrorMessage("An unknown error occurred");
+        }
+      }
+    },
+  });
+
+  // const signIn = async () => {
+  //   try {
+  //     await createUserWithEmailAndPassword(auth, email, password);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
+
+  // const signInWithGoogle = async () => {
+  //   try {
+  //     await signInWithPopup(auth, googleProvider);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
 
   const logout = async () => {
     try {
@@ -37,26 +87,41 @@ export const Auth = () => {
   };
 
   return (
-    <>
-      <Flex {...loginContainerStyles}>
+    <Flex {...loginContainerStyles}>
+      <form onSubmit={formik.handleSubmit}>
         <VStack spacing="2rem" pb="3rem">
-          <Heading>Logga in</Heading>
+          <Heading>Admin Login</Heading>
+
           <Input
-            placeholder="Email..."
-            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            id="email"
+            name="email"
+            type="email"
+            onChange={formik.handleChange}
+            value={formik.values.email}
           />
+          {formik.touched.email && formik.errors.email ? (
+            <div>{formik.errors.email}</div>
+          ) : null}
+
           <Input
-            placeholder="Password..."
+            placeholder="Password"
+            id="password"
+            name="password"
             type="password"
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={formik.handleChange}
+            value={formik.values.password}
           />
+          {formik.touched.password && formik.errors.password ? (
+            <div>{formik.errors.password}</div>
+          ) : null}
         </VStack>
         <HStack spacing="2rem">
-          <Button onClick={signIn}>Logga in</Button>
-          <Button onClick={signInWithGoogle}>Logga in med Google</Button>
+          <Button type="submit">Login</Button>
           <Button onClick={logout}>Logout</Button>
         </HStack>
-      </Flex>
-    </>
+      </form>
+      {errorMessage && <div>{errorMessage}</div>}
+    </Flex>
   );
 };
