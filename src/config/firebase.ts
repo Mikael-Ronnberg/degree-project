c284c618a44df2681d8b53e27686253ae1da6657
Mikael Rönnberg <mikael.rnnberg@gmail.com>
2023-12-19 19:36:16 +0100

    setting up env

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,21 +1,22 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyD__KHG6ix82QmjF5ROeeUt59L-7A5yguo",
-  authDomain: "svepabotten.firebaseapp.com",
-  projectId: "svepabotten",
-  storageBucket: "svepabotten.appspot.com",
-  messagingSenderId: "942412613736",
-  appId: "1:942412613736:web:6fd87036a49482decfd3dd",
-  measurementId: "G-TQGW72PNJ2",
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REAC_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
 };
 
 // Initialize Firebase
